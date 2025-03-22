9a7185573e1180e11aec9dc82b21d233ecfc2e66
DevMevTV <devidfitz533@gmail.com>
2025-03-22 20:38:55 +0100

    nodejs stuff, yk

--- src/client/java/de/devmevtv/jsmc/client/Javet.java
+++ src/client/java/de/devmevtv/jsmc/client/Javet.java
@@ -8,29 +8,53 @@ import com.caoccao.javet.interop.engine.IJavetEngine;
 import com.caoccao.javet.interop.engine.IJavetEnginePool;
 import com.caoccao.javet.interop.engine.JavetEngineConfig;
 import com.caoccao.javet.interop.engine.JavetEnginePool;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Javet {
-    public static void run() {
+
+    public static NodeRuntime runtime;
+
+    static {
+        init();
+    }
+
+    public static void init() {
+        IJavetEnginePool<NodeRuntime> enginePool = new JavetEnginePool<>(
+                new JavetEngineConfig().setJSRuntimeType(JSRuntimeType.Node)
+        );
+
+        try {
+            runtime = enginePool.getEngine().getV8Runtime();
+
+            runtime.setConverter(new JavetProxyConverter());
+            runtime.getGlobalObject().set("console", Console.class);
+            runtime.getGlobalObject().set("world", World.class);
+
+        } catch (JavetException e) {
+            Console.error(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void run(Path project) {
         new Thread(() -> {
-            try (IJavetEnginePool<NodeRuntime> enginePool = new JavetEnginePool<>(
-                    new JavetEngineConfig().setJSRuntimeType(JSRuntimeType.Node)
-            )) {
-                try (IJavetEngine<NodeRuntime> engine = enginePool.getEngine()) {
-                    NodeRuntime runtime = engine.getV8Runtime();
+            try {
+                JSONObject json = new JSONObject(Files.readString(project.resolve("package.json")));
 
-                    runtime.setConverter(new JavetProxyConverter());
-
-                    try (var globalObject = runtime.getGlobalObject()) {
-                        globalObject.set("console", Console.class);
-                        runtime.getExecutor(FabricLoader.getInstance().getGameDir().resolve("test").resolve("index.js")).executeVoid();
-                        runtime.getGlobalObject().delete("console");
-                    }
-                }
+                runtime.getExecutor(project.resolve(json.getString("main"))).executeVoid();
             } catch (JavetException e) {
-                throw new RuntimeException(e);
+                Console.error(e.getMessage());
+                e.printStackTrace();
+            } catch (IOException e) {
+                Console.error("Missing package.json in project");
+                e.printStackTrace();
             }
         }).start();
     }
@@ -38,6 +62,30 @@ public class Javet {
     public final class Console {
         public static void log(String msg) {
             MinecraftClient.getInstance().player.sendMessage(Text.of(msg), false);
+        }
+
+        public static void info(String msg) {
+            MinecraftClient.getInstance().player.sendMessage(Text.literal("[Info] " + msg).formatted(Formatting.GREEN), false);
+        }
+
+        public static void warn(String msg) {
+            MinecraftClient.getInstance().player.sendMessage(Text.literal("[Warn] " + msg).formatted(Formatting.YELLOW), false);
+        }
+
+        public static void error(String msg) {
+            MinecraftClient.getInstance().player.sendMessage(Text.literal("[Error] " + msg).formatted(Formatting.RED), false);
+        }
+    }
+
+    public static final class World {
+        private World() {}
+
+        public static void write(String msg) {
+            MinecraftClient.getInstance().getNetworkHandler().sendChatMessage(msg);
+        }
+
+        public static void onTick(Runnable callback) {
+            JsMcClient.addEvent("Tick", callback);
         }
     }
 }
--- src/client/java/de/devmevtv/jsmc/client/JsMcClient.java
+++ src/client/java/de/devmevtv/jsmc/client/JsMcClient.java
@@ -1,19 +1,104 @@
 package de.devmevtv.jsmc.client;
 
+import com.caoccao.javet.exceptions.JavetException;
 import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 
 public class JsMcClient implements ClientModInitializer {
+
+    private static final Map<String, List<Runnable>> Events = new HashMap<>();
+    public static Path ScriptsFolder;
+
     @Override
     public void onInitializeClient() {
+        ScriptsFolder = FabricLoader.getInstance().getGameDir().resolve("jsrunner");
+
+        try {
+            if (!Files.exists(ScriptsFolder))
+                Files.createDirectory(ScriptsFolder);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, access) -> {
-            dispatcher.register(ClientCommandManager.literal("run").executes((context) -> {
-                Javet.run();
+
+            dispatcher.register(ClientCommandManager.literal("javascriptrunner").then(ClientCommandManager.literal("new").then(ClientCommandManager.argument("name", StringArgumentType.word()).executes(context -> {
+                String name = StringArgumentType.getString(context, "name");
+                Path project = ScriptsFolder.resolve(name);
+                if (Files.exists(project)) {
+                    MinecraftClient.getInstance().player.sendMessage(Text.of("§cProject " + name + " already exists"), false);
+                    return Command.SINGLE_SUCCESS;
+                }
+                try {
+                    Files.createDirectory(project);
+                    Files.writeString(project.resolve("main.js"), "console.log('Hello, World')");
+                    Files.writeString(project.resolve("package.json"), "{" +
+                            "\"name\":\"" + name + "\"," +
+                            "\"version:\": \"1.0.0\"," +
+                            "\"description\": \"\"," +
+                            "\"main\":\"main.js\"" +
+                            "\"scripts\": {" +
+                            "\"    \"test\": \"echo \\\"Error: no test specified\\\" && exit 1\"" +
+                            "\"}," +
+                            "\"keywords\": []," +
+                            "\"author\": \"\"," +
+                            "\"license\": \"ISC\"" +
+                            "}");
+                } catch (IOException e) {
+                    MinecraftClient.getInstance().player.sendMessage(Text.of("§cFailed to create project" + name), false);
+                    e.printStackTrace();
+                }
+                MinecraftClient.getInstance().player.sendMessage(Text.of("§aSuccessfully created " + name), false);
                 return Command.SINGLE_SUCCESS;
-            }));
+            }))).then(ClientCommandManager.literal("run").then(ClientCommandManager.argument("name", StringArgumentType.word()).executes(context -> {
+                String name = StringArgumentType.getString(context, "name");
+                Path project = ScriptsFolder.resolve(name);
+                if (!Files.exists(project)) {
+                    MinecraftClient.getInstance().player.sendMessage(Text.of("§cProject " + name + " doesn't exist"), false);
+                    return Command.SINGLE_SUCCESS;
+                }
+                Javet.run(project);
+                return Command.SINGLE_SUCCESS;
+            }))).then(ClientCommandManager.literal("stop").executes(context -> {
+                try {
+                    Javet.runtime.close(true);
+                    Javet.runtime.removeCallbackContext(0);
+                } catch (JavetException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    Javet.init();
+                }
+                return Command.SINGLE_SUCCESS;
+            })));
+            dispatcher.register(ClientCommandManager.literal("jsrunner").redirect(dispatcher.findNode(Collections.singleton("javascriptrunner"))));
+            dispatcher.register(ClientCommandManager.literal("jsr").redirect(dispatcher.findNode(Collections.singleton("javascriptrunner"))));
         });
+
+        ClientTickEvents.START_CLIENT_TICK.register(a -> handleEvent("Tick"));
+    }
+
+    public static void handleEvent(String event) {
+        Events.computeIfPresent(event, (key, list) -> {
+            Events.get("Tick").forEach(Runnable::run);
+            return list;
+        });
+    }
+
+    public static void addEvent(String event, Runnable callback) {
+        Events.computeIfAbsent(event, a -> new ArrayList<>());
+
+        Events.get(event).add(callback);
     }
 }
